ff673bcf14e9ce3946e0c39080e9041da7aa4d48
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2019-12-05 13:58:31 -0600

    update home page

--- src/app.py
+++ src/app.py
@@ -1,15 +1,25 @@
 from flask import Flask, request, render_template
 import redis
 
-dbMovies = redis.Redis(db = 0)
-dbGenres = redis.Redis(db = 1)
+dbMovies = redis.Redis(charset="utf-8", decode_responses=True, db = 0)
+dbGenres = redis.Redis(charset="utf-8", decode_responses=True, db = 1)
+
 app = Flask(__name__)
 
 @app.route('/home')
 def index():
-    search = request.args.get('search', "no hay coincideincias")
-    busqueda = str(dbGenres.smembers(search))
-    return render_template('home.html', busqueda = busqueda)
+    listMov = [] #Lista donde almacenamos todas las peliculas, las almacenamos como str
+    
+    search = request.args.get('search', "no hay coincideincias") #Almacenamos los datos ingresados en el input
+
+    searchMov = ' '
+    for x in range (1, 9742):
+        for movie in dbMovies.smembers(x):
+            listMov.append(movie) #Agregamos cada pelicula a la lista
+            if (search == movie):
+                searchMov = 'si tenemos la pelicula: {}'.format(movie)
+        
+    return render_template('home.html', list = listMov, result = searchMov)
 
 @app.route('/list')
 def listMov():
