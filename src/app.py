edaa7e33cac635ace8c30a27ef4f04ebd5d8048a
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2019-12-06 01:24:30 -0600

    finish the WebApp principal funtions

--- src/app.py
+++ src/app.py
@@ -3,11 +3,14 @@ import redis
 
 dbMovies = redis.Redis(charset="utf-8", decode_responses=True, db = 0)
 dbGenres = redis.Redis(charset="utf-8", decode_responses=True, db = 1)
+dbGenresM = redis.Redis(charset="utf-8", decode_responses=True, db = 2)
+dbUsers = redis.Redis(charset="utf-8", decode_responses=True, db = 3)
 
 app = Flask(__name__)
 
-@app.route('/home')
-def index():
+@app.route('/home/<string:addMovie>')
+@app.route('/home') #Pagina de inicio donde mostramos el catalogo completo
+def index(addMovie = None):
     listMov = [] #Lista donde almacenamos todas las peliculas, las almacenamos como str
     
     search = request.args.get('search', "no hay coincideincias") #Almacenamos los datos ingresados en el input
@@ -17,13 +20,44 @@ def index():
         for movie in dbMovies.smembers(x):
             listMov.append(movie) #Agregamos cada pelicula a la lista
             if (search == movie):
-                searchMov = 'si tenemos la pelicula: {}'.format(movie)
-        
+                searchMov = movie
+
+    if(addMovie != None and addMovie != ' '):
+        dbUsers.sadd('u1', addMovie)
+
     return render_template('home.html', list = listMov, result = searchMov)
 
-@app.route('/list')
-def listMov():
-    return render_template('list.html')
+
+@app.route('/list/<string:delMov>')
+@app.route('/list') #Lista de peliculas del usuario
+def listMov(delMov = None):
+    listMov = [] #Lista de peliculas del usuario
+
+    if(delMov != None):
+        dbUsers.srem('u1', delMov)
+
+    for movie in dbUsers.smembers('u1'):
+       listMov.append(movie) 
+        
+    return render_template('list.html', list = listMov)
+
+@app.route('/listRecInt')
+def ListRecomInter():
+    listMov = [] #Lista de peliculas del usuario
+    listRec = [] #Lista de peliculas recomendadas
+    for movie in dbUsers.smembers('u1'): #Almacenamos en listMov las peliculas asociadas al usuario
+       listMov.append(movie) 
+
+    if(len(listMov) != 0):
+        interGenres = dbGenres.sinter(listMov) #Almacenamos los generos que coincidan en la variable
+        print(interGenres)
+        for genre in interGenres:
+            for idMovie in dbGenresM.smembers(genre):
+                for movie in dbMovies.smembers(idMovie):
+                    listRec.append(movie)
+
+    return render_template('listRecInt.html', listMov = listRec)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/conf.py
+++ src/conf.py
@@ -3,21 +3,15 @@ import redis
 
 mov = pd.read_csv('movies.csv') 
 dbMovies = redis.Redis(db = 0) 
-dbGenres = redis.Redis(db = 1) 
-
-dicGen = {'Action':[], 'Adventure':[], 'Animation':[], 'Comedy':[], 'Crime':[], 'Documentary':[],
-            'Drama':[], 'Fantasy':[], 'Film-Noir':[], 'Horror':[], 'Musical':[], 'Mystery':[], 'Romance':[],
-             'Sci-Fi':[], 'Thriller':[], 'War':[], 'Western':[]} 
-
-genres = dicGen.keys()
+dbGenres = redis.Redis(db = 1)
+dbGenresM = redis.Redis(db = 2)
 
 for index, row in mov.iterrows():
-    
     listGen = row['genres'].split('|')
     for genre in listGen:
         dbGenres.sadd(row['title'], genre)
+        dbGenresM.sadd(genre, row['movieId'])
     
     dbMovies.sadd(row['movieId'], row['title'])
     
-print(dbGenres.smembers('Toy Story (1995)')) 
 print('done')
