51efb609220e5053ee7b861fc92f8bcc513e21b5
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2019-12-06 09:22:21 -0600

    update

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
 import redis
 
+#Variables donde almacenamos los clientes de la DB que ingresaran y extraeran informacion
 dbMovies = redis.Redis(charset="utf-8", decode_responses=True, db = 0)
 dbGenres = redis.Redis(charset="utf-8", decode_responses=True, db = 1)
 dbGenresM = redis.Redis(charset="utf-8", decode_responses=True, db = 2)
 dbUsers = redis.Redis(charset="utf-8", decode_responses=True, db = 3)
+
 
 app = Flask(__name__)
 
@@ -19,7 +21,7 @@ def index(addMovie = None):
     for x in range (1, 9742):
         for movie in dbMovies.smembers(x):
             listMov.append(movie) #Agregamos cada pelicula a la lista
-            if (search == movie):
+            if (search == movie): #Validacion de coincideincia de la busqueda
                 searchMov = movie
 
     if(addMovie != None and addMovie != ' '):
@@ -41,8 +43,9 @@ def listMov(delMov = None):
         
     return render_template('list.html', list = listMov)
 
+@app.route('/listRecInt/<string:addMovie>')
 @app.route('/listRecInt')
-def ListRecomInter():
+def ListRecomInter(addMovie = None):
     listMov = [] #Lista de peliculas del usuario
     listRec = [] #Lista de peliculas recomendadas
     for movie in dbUsers.smembers('u1'): #Almacenamos en listMov las peliculas asociadas al usuario
@@ -56,8 +59,31 @@ def ListRecomInter():
                 for movie in dbMovies.smembers(idMovie):
                     listRec.append(movie)
 
+    if(addMovie != None):
+        dbUsers.sadd('u1', addMovie)
+
     return render_template('listRecInt.html', listMov = listRec)
 
+@app.route('/listRecUn/<string:addMovie>')
+@app.route('/listRecUn')
+def ListRecomUnion(addMovie = None):
+    listMov = [] #Lista de peliculas del usuario
+    listRec = [] #Lista de peliculas recomendadas
+    for movie in dbUsers.smembers('u1'): #Almacenamos en listMov las peliculas asociadas al usuario
+       listMov.append(movie) 
+
+    if(len(listMov) != 0):
+        interGenres = dbGenres.sunion(listMov) #Almacenamos los generos que coincidan en la variable
+        print(interGenres)
+        for genre in interGenres:
+            for idMovie in dbGenresM.smembers(genre):
+                for movie in dbMovies.smembers(idMovie):
+                    listRec.append(movie)
+    
+    if(addMovie != None):
+        dbUsers.sadd('u1', addMovie)
+
+    return render_template('listRecUn.html', listMov = listRec)
 
 if __name__ == '__main__':
     app.run(debug=True)
